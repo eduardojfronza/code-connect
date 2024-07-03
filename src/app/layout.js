a3a892d46e5d8201dbc4b735f67390fb44aebe17
Eduardo <edujfronza@gmail.com>
2024-07-03 20:53:25 -0300

    feat - implemented compponent aside, avatar and cardpost

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Aside } from "./components/Aside";
+import { Aside } from "../components/Aside";
 import "./globals.css";
 
 export const metadata = {
---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export const Avatar = ({name, imageSrc}) => {
+    return(
+        <ul>
+            <li><Image src={imageSrc} alt={`Avatar do(a) ${name}`} width={32} height={32}/></li>
+            <li>@{name}</li>
+        </ul>
+    )
+}
