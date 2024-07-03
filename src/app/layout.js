943f764cddf0f0a7f1625e2d709e5d3b2bf234db
Eduardo <edujfronza@gmail.com>
2024-07-03 20:24:22 -0300

    feat - first styles

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import { Aside } from './components/Aside';
+import './globals.css'
+
 export const metadata = {
   title: "Code Connect",
   description: "Uma rede social para devs!",
@@ -6,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        <Aside />
+        {children}
+        aas
+        </body>
     </html>
   );
 }
