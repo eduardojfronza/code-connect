24d18beb54ab46f5111699143c1dcf552a6999d4
Eduardo <edujfronza@gmail.com>
2024-07-05 15:43:55 -0300

    feat - creating seed

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,37 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 import { CardPost } from "@/components/CardPost";
 import logger from "@/logger";
-import Link from 'next/link';
+import Link from "next/link";
+import db from "../../prisma/db";
 
 async function getAllPosts(page) {
-  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
-  .catch(error => {
-    logger.error('Erro de rede: ' + error.message);
-    return null;
-  });
+  try {
+    const posts = await db.post.findMany()
 
-  if(!response) {
-    logger.error('Ops, alguma coisa aconteceu!')
-    return []
+    return {data: posts, prev: null, next: null}
+
+  } catch (error) {
+
+    logger.error('Falha ao obter posts', { error })
+    return {data: [], prev: null, next: null}
   }
-
-  logger.info("Posts obtidos com sucesso!")
-  return response.json()
 }
 
 export default async function Home({ searchParams }) {
-  const currentPage = searchParams?.page || 1
-  const {data: posts, prev, next} = await getAllPosts(currentPage)
-  
+  const currentPage = searchParams?.page || 1;
+  const { data: posts, prev, next } = await getAllPosts(currentPage);
+
   return (
     <main className={styles.grid}>
-      {posts.map(post =>  <CardPost post={post} key={post.id}/>)}
+      {posts.map((post) => (
+        <CardPost post={post} key={post.id} />
+      ))}
 
-    <div className={styles.links}>
-      {prev && <Link href={`/?page=${prev}`}>Página anteriror</Link>}
-      {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
-    </div>
+      <div className={styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página anteriror</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   );
 }
