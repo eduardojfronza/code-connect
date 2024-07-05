c82209412f0eb45db140336c670a505f7f1f5a65
Eduardo <edujfronza@gmail.com>
2024-07-05 11:20:18 -0300

    fix - fixing styles

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image"
+import styles from "./avatar.module.css"
 
 export const Avatar = ({name, imageSrc}) => {
     return(
-        <ul>
+        <ul className={styles.avatar}>
             <li><Image src={imageSrc} alt={`Avatar do(a) ${name}`} width={32} height={32}/></li>
             <li>@{name}</li>
         </ul>
