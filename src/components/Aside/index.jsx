302ff60f07404d4b5f766728b675ad4be09ce999
Eduardo <edujfronza@gmail.com>
2024-07-06 11:12:40 -0300

    refactor - improving styles

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,10 +1,20 @@
-import Image from "next/image"
-import styles from './aside.module.css'
+import Image from "next/image";
+import styles from "./aside.module.css";
+import Link from "next/link";
 
 export const Aside = () => {
-    return(
-        <aside className={styles.aside}>
-            <Image priority src={'/assets/logo.png'} alt="Logo da code connect" width={128} height={40}  objectFit="contain"/>
-        </aside>
-    )
-}
+  return (
+    <aside className={styles.aside}>
+      <Link href="/">
+        <Image
+          priority
+          src={"/assets/logo.png"}
+          alt="Logo da code connect"
+          width={128}
+          height={40}
+          objectFit="contain"
+        />
+      </Link>
+    </aside>
+  );
+};
